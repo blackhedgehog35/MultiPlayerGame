366ce6a29a2c3d41fe522ffc06a82345c36a4c79
MaxDkn <max.deckmyn@gmail.com>
2024-06-05 15:20:07 +0200

    J'ai fait la fonction update dans main-game

--- game-main.py
+++ game-main.py
@@ -1,17 +1,37 @@
 import sys
 import pygame
+
+import game.client
 from game import player
 
 
 class Game:
     FPS = 20
 
-    def __init__(self, ):
-        pygame.init()  # Initialize pygame here
+    def __init__(self, connection: game.client.ClientNetwork):
+        self.conn = connection
+        pygame.init()
         self.screen = pygame.display.set_mode((1100, 620))
         pygame.display.set_caption('')
         self.clock = pygame.time.Clock()
-        self.sprites = [player.Player(self.screen, 'ffffff', (0, 0))]
+        self.sprites = {self.conn.KEY: player.Player(self.screen, self.conn.KEY, self.conn.spawn_pos)}
+
+    def draw_bg(self):
+        self.screen.fill('#a4a4a4')
+
+    def update(self, dt, server_game):
+        self.draw_bg()
+        self.sprites[self.conn.KEY].input()
+        for key in server_game.keys():
+            if key not in self.sprites.keys():
+                self.sprites[key] = game.player.Player(self.screen, key, server_game[key]['pos'])
+            else:
+                if key != self.conn.KEY:
+                    self.sprites[key].set_attributs(server_game[key]['pos'])
+
+        for sprite in self.sprites.values():
+            sprite.update(dt)
+            sprite.draw()
 
     def run(self):
         run = True
@@ -21,16 +41,12 @@ class Game:
                 if event.type == pygame.QUIT:
                     run = False
 
-            self.screen.fill('green')
-            self.sprites[0].input()
-            for sprite in self.sprites:
-                sprite.update(dt)
-                sprite.draw()
+            server_game = self.conn.send_attribute((self.sprites[self.conn.KEY].rect.x, self.sprites[self.conn.KEY].rect.y))
+            self.update(dt, server_game)
             pygame.display.flip()
 
         pygame.quit()
         sys.exit()
 
-
 if __name__ == '__main__':
-    Game().run()
+    Game(game.client.ClientNetwork("192.168.1.69", 3010)).run()
