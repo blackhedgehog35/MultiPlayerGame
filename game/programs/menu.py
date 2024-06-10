37d19dea767e2704b197cc05d9ddcc323f0989ca
Yoan <yoan.vandenbussche@gmail.com>
2024-06-10 18:47:02 +0200

    I began to add the selector class I going to explain if you don't understand things, it's a bit of a messed up

--- game/programs/menu.py
+++ game/programs/menu.py
@@ -1,5 +1,5 @@
 import pygame
-from ui import Text, Input, Button
+from ui import *
 from level import Level
 from client import ClientNetwork
 from config import ConfigFile
@@ -13,10 +13,12 @@ class MainWindow:
     def __init__(self) -> None:
         self.screen = pygame.display.set_mode((self.width, self.height))
         pygame.display.set_caption('MENU - MULTIPLAYER GAME')
-        self.text = Text('Press <Ctrl> and <Tab> to start the game', 50, (550, 400), 'black', 'center')
+        self.text = Text(self.screen, 'Press <Ctrl> and <Tab> to start the game', 50, (550, 400), 'black', 'center')
         self.game = None
-        self.input = Input(self.screen, (300, 100), (390, 500), (255, 255, 255), "rounded rect", 50)
-        self.button = Button(self.screen, (100, 100), (600, 300), (0, 0, 0), "rounded rect", [pygame.quit])
+        self.input = Input(self.screen, (300, 100), (390, 500), (255, 255, 255), "rounded rect", 50, radius=25)
+        self.button = Button(self.screen, (100, 100), (600, 300), (255, 0, 0), "rounded rect", [pygame.quit], radius=25)
+        self.selector = Selector(self.screen, (600, 600), [Text(self.screen, "hgffdgfdgfdgfd", 25, (0, 0)),
+                                                                       Text(self.screen, "hgfh", 25, (0, 0), (0, 0, 0))])
 
     def draw_background(self):
         self.screen.fill(self.background_color)
@@ -47,11 +49,14 @@ class MainWindow:
                         self.input.is_writing = False
 
                     self.button.check_clicked(event)
+                    self.selector.left_arrow.check_clicked(event)
+                    self.selector.right_arrow.check_clicked(event)
 
             self.draw_background()
-            self.text.draw(self.screen)
+            self.text.draw()
             self.input.draw()
             self.button.draw()
+            self.selector.draw()
             self.input.display_data()
             pygame.display.update()
             self.screen.fill(self.background_color)
--- game/programs/ui.py
+++ game/programs/ui.py
@@ -3,7 +3,7 @@ import pygame
 
 class Shapes:
 
-    def __init__(self, screen, size: tuple, pos: tuple, color, shape, image):
+    def __init__(self, screen, size: tuple, pos: tuple, color, shape, image, radius):
         self.screen = screen
         self.shape = shape
         self.color = color
@@ -11,14 +11,14 @@ class Shapes:
         self.image = image
         self.rect = pygame.Rect(pos, self.size)
         self.all_shapes = {
-            "rect": pygame.draw.rect,
-            "cercle": pygame.draw.circle,
-            "line": pygame.draw.line,
-            "rounded rect": self.draw_rounded_rectangle
+            "rect": lambda: pygame.draw.rect(self.screen, self.color, self.rect),
+            "circle": lambda: pygame.draw.circle(self.screen, self.color, (self.rect.x, self.rect.y), radius),
+            "line": lambda: pygame.draw.line(self.screen, self.color, self.rect, (self.rect.x + size[0], self.rect.y + self.size[1])),
+            "rounded rect": lambda: self.draw_rounded_rectangle(self.screen, self.color, self.rect, radius),
+            "none": lambda: self.draw_nothing()
         }
 
-    def draw_rounded_rectangle(self, screen, color, rect):
-        radius = 25
+    def draw_rounded_rectangle(self, screen, color, rect, radius):
         x, y, width, height = rect
 
         pygame.draw.rect(screen, color, (x + radius, y, width - 2 * radius, height))
@@ -29,8 +29,11 @@ class Shapes:
         pygame.draw.circle(screen, color, (x + radius, y + height - radius), radius)
         pygame.draw.circle(screen, color, (x + width - radius, y + height - radius,), radius)
 
+    def draw_nothing(self):
+        pass
+
     def draw(self):
-        self.all_shapes.get(self.shape)(self.screen, self.color, self.rect)
+        self.all_shapes.get(self.shape)()
         if self.image is not None:
             self.image.draw()
 
@@ -38,7 +41,8 @@ class Shapes:
 class Text:
     font_name = "COMIC SANS MS"
 
-    def __init__(self, text, size, pos, color=(255, 255, 255), side='center'):
+    def __init__(self, screen, text, size, pos, color=(255, 255, 255), side='center'):
+        self.screen = screen
         self.color = color
         self.text_str = text
         self.size = size
@@ -46,21 +50,22 @@ class Text:
         self.pos = pos
         self.side = side
 
-        self.text = self.font.render(str(self.text_str), True, self.color)
-        self.rect = self.text.get_rect()
+        self.data = self.font.render(str(self.text_str), True, self.color)  # data because for the selector we need
+        # the same name for texts and images
+        self.rect = self.data.get_rect()
         setattr(self.rect, self.side, self.pos)
 
     def update_text(self, text_str):
         self.text_str = text_str
-        self.text = self.font.render(str(self.text_str), True, self.color)
-        self.rect = self.text.get_rect()
+        self.data = self.font.render(str(self.text_str), True, self.color)
+        self.rect = self.data.get_rect()
         setattr(self.rect, self.side, self.pos)
 
     def update_pos(self, pos):
         setattr(self.rect, self.side, pos)
 
-    def draw(self, screen: pygame.surface.Surface):
-        screen.blit(self.text, self.rect)
+    def draw(self):
+        self.screen.blit(self.data, self.rect)
 
 
 class Images:
@@ -76,18 +81,18 @@ class Images:
 
 class Input(Shapes):
 
-    def __init__(self, screen, size: tuple, pos: tuple, color, shape: str, data_size, image=None):
-        super().__init__(screen, size, pos, color, shape, image)
+    def __init__(self, screen, size: tuple, pos: tuple, color, shape: str, data_size, image=None, radius=5):
+        super().__init__(screen, size, pos, color, shape, image, radius)
         self.data = ""
         self.is_writing = False
         self.data_size = data_size
-        self.data_text = Text(self.data, self.data_size, (self.rect.x + 5, self.rect.y + 5), (0, 0, 0))
+        self.data_text = Text(self.screen, self.data, self.data_size, (self.rect.x + 5, self.rect.y + 5), (0, 0, 0))
 
     def write(self, key_pressed):
         if key_pressed == pygame.K_BACKSPACE:
             self.data = self.data[0:-1]
 
-        elif self.data_text.text.get_size()[0] + 20 < self.size[0]:
+        elif self.data_text.data.get_size()[0] + 20 < self.size[0]:
             self.data += chr(key_pressed)
         self.data_text.update_text(self.data)
 
@@ -99,13 +104,13 @@ class Input(Shapes):
         data = self.data
 
     def display_data(self):
-        self.screen.blit(self.data_text.text, (self.rect.x + 5, self.rect.y + 5))
+        self.screen.blit(self.data_text.data, (self.rect.x + 5, self.rect.y + 5))
 
 
 class Button(Shapes):
 
-    def __init__(self, screen, size: tuple, pos: tuple, color, shape: str, effect: list, image=None):
-        super().__init__(screen, size, pos, color, shape, image)
+    def __init__(self, screen, size: tuple, pos: tuple, color, shape: str, effect: list, image=None, radius=5):
+        super().__init__(screen, size, pos, color, shape, image, radius)
         self.all_effects = effect
 
     def click(self):
@@ -115,3 +120,41 @@ class Button(Shapes):
     def check_clicked(self, event):
         if self.rect.collidepoint(event.pos):
             self.click()
+
+
+class Selector:
+
+    def __init__(self, screen, pos, list_to_display: list, shape=None):
+        self.screen = screen
+        self.list_to_display = list_to_display
+        self.size = self.define_size()
+        self.rect = pygame.Rect(pos, self.size)
+        self.number = 0
+        self.shape = shape
+        self.left_arrow = Button(self.screen, (self.size[0] / 2, self.size[1] / 2), (self.rect.x - self.size[0] / 2 - 50, self.rect.y), (0, 0, 0)
+                                 , "circle", [lambda: self.add_number(-1)])
+        self.right_arrow = Button(self.screen, (self.size[0] / 2, self.size[1] / 2), (self.rect.x + self.size[0] + 50, self.rect.y), (0, 0, 0)
+                                  , "circle", [lambda: self.add_number(1)])
+
+    def define_size(self):
+        all_data_size = []
+
+        for data in self.list_to_display:
+            all_data_size.append(data.data.get_size())
+
+        all_data_size.sort()
+        return all_data_size[-1]
+
+
+    def draw(self):
+        self.screen.blit(self.list_to_display[self.number].data, self.rect)
+        self.right_arrow.draw()
+        self.left_arrow.draw()
+
+    def add_number(self, amount):
+        self.number += amount
+        if self.number >= len(self.list_to_display):
+            self.number = 0
+        elif self.number <= -1:
+            self.number = len(self.list_to_display) - 1
+
